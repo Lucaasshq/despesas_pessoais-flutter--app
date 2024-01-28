5d812c083fb886fc65c52597262624e25e3293df
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-01-28 00:00:25 -0300

    .

---
+++ despesas_pessoais/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 124, 33, 243),
+        title: const Text(
+          'Despesas Pessoais',
+          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255),
+          fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: const Column(
+        children: <Widget> [
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Gráfico'),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Text('Lista de Transações')
+            ,
+          )
+        ],
+      ),
+    );
+  }
+}
