6fbee4fec8160f6856b38ac12fd8ef2d9c4b21f7
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-02-01 08:17:27 -0300

    .

---
+++ despesas_pessoais/lib/components/transaction_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionsList extends StatelessWidget {
+  const TransactionsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
